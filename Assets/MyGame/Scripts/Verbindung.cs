9b867991f4f80d4ca5a3826a74850071703de37e
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2026-01-30 14:16:32 +0100

    🌸 Add data model for grid and input line

---
+++ Assets/MyGame/Scripts/Verbindung.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Verbindung : MonoBehaviour
+{
+    public bool[] Eingabezeilen = new bool[7];
+    public bool[,] Raster = new bool[10, 7];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    
+    }
+}
